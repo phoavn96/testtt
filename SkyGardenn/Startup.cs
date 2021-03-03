214679d31079181d7a1a7c6c8daa5d16ec307135
phoavn96 <12345Bien>
2021-03-03 18:56:00 +0700

    test

---
+++ SkyGardenn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyGardenn.Startup))]
+namespace SkyGardenn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
